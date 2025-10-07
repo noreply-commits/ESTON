e76f187e80b84e60453373e63a1a887e7cfbbd81
noreply-commits <noreply@eston.edu.gh>
2025-10-07 16:00:48 +0000

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,18 +1,19 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  host: 'db.nvlnyhyldrpuvhjwfjxc.supabase.co',
+  port: 5432,
+  database: 'postgres',
+  user: 'postgres',
+  password: 'Techland@244190',
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
   ssl: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: false, // Important for Supabase SSL
   },
 });
+
 
 
 
