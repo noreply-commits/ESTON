d7cd022479452c554781885ab1d24610fa6f38bd
noreply-commits <noreply@eston.edu.gh>
2025-10-07 16:46:17 +0000

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,13 +1,16 @@
+const dns = require('dns');
 const { Pool } = require('pg');
-const bcrypt = require('bcryptjs');
 
 const pool = new Pool({
   user: 'postgres',
   password: 'Techland@244190',
   host: 'db.nvlnyhyldrpuvhjwfjxc.supabase.co',
   port: 5432,
-  database: 'postgres',
   ssl: {
     rejectUnauthorized: false,
   },
+  lookup: (hostname, options, callback) => {
+    // Force IPv4 addresses only
+    return dns.lookup(hostname, { family: 4 }, callback);
+  }
 });
